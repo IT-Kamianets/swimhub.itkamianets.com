9c67cfc88f9a5cd2fc3c6d134a82decf96a95479
sixstyyy <sdfysd1308@gmail.com>
2026-04-28 00:39:25 +0300

    Фінальна правка: залишено 4 мови

---
+++ work/meni-by-mnyasa.itkamianets.com/src/app/feature/language/language.const.ts
@@ -0,0 +1,28 @@
+import { LanguageOption } from './language.interface';
+
+export const LANGUAGES: LanguageOption[] = [
+	{
+		code: 'ua',
+		label: 'Українська',
+		flagSrc: 'flags/ukraine.svg',
+		htmlLang: 'uk',
+	},
+	{
+		code: 'en',
+		label: 'English',
+		flagSrc: 'flags/united-kingdom.svg',
+		htmlLang: 'en',
+	},
+	{
+		code: 'de',
+		label: 'Deutsch',
+		flagSrc: 'flags/germany.svg',
+		htmlLang: 'de',
+	},
+	{
+		code: 'fr',
+		label: 'Français',
+		flagSrc: 'flags/france.svg',
+		htmlLang: 'fr',
+	},
+	];
